b535a24a7f4a97201aab906e916a0a0688619671
Edgares100IQ <edgar.08ds@gmail.com>
2026-02-01 18:32:23 +0100

    Add files via upload

---
+++ script-full.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo -v
+echo "$USER ALL=(ALL) NOPASSWD: /usr/bin/pacman" | sudo tee /etc/sudoers.d/99-pacman-nopasswd
+sudo chmod 440 /etc/sudoers.d/99-pacman-nopasswd
+
+sudo pacman -Sy archlinux-keyring --noconfirm
+sudo pacman -Syu --noconfirm
+
+sudo pacman -S --needed gcc cmake git curl perl wget base-devel rust go sassc flatpak libreoffice-fresh gimp discord blender noto-fonts ttf-dejavu ttf-liberation ttf-ubuntu-font-family ttf-fira-sans --noconfirm
+
+if ! command -v yay &> /dev/null; then
+    cd /tmp && rm -rf yay-bin
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin && makepkg -si --noconfirm
+fi
+
+yay -S --noconfirm --needed cava pavucontrol-qt wireplumber pipewire-pulse libdbusmenu-gtk3 playerctl
+
+cd ~/.cache
+rm -rf dots-hyprland
+git clone --depth 1 https://github.com/end-4/dots-hyprland
+cd dots-hyprland
+chmod +x setup
+(printf '\n\n\n\nn\nyesforall\n'; sleep 5; while true; do echo "y"; sleep 2; done) | ./setup install || true
+
+cd ~
+sudo sed -i '/^OPTIONS=/c\OPTIONS=(!strip docs libtool staticlibs emptydirs zipman purge !debug lto)' /etc/makepkg.conf
+rm -rf ~/.cache/yay/vscodium-bin
+
+yay -S --noconfirm --needed --answerclean All --answerdiff None opera
+yay -S --noconfirm --needed --answerclean All --answerdiff None heroic-games-launcher-bin
+env -u MAKEPKGFLAGS yay -S --noconfirm --needed --answerclean All --answerdiff None vscodium-bin --mflags "--nocheck --skipinteg"
+
+flatpak install flathub com.valvesoftware.Steam -y
+
+sudo rm -f /etc/sudoers.d/99-pacman-nopasswd
+
+for i in {10..1}; do
+  echo "Reboot en $i"
+  sleep 1
+done
+sudo reboot
