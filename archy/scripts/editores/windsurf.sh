5787eeb85747a1a9b1b0bbde30f17f6d5ca38c86
Edgares100IQ <edgar.08ds@gmail.com>
2026-04-25 14:15:52 +0200

    Delete archy directory

--- archy/scripts/editores/windsurf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-echo ">>> Instalando Windsurf..."
-yay -S --noconfirm --needed --answerclean All --answerdiff None windsurf
-
-echo ">>> Windsurf instalado"
--- archy/scripts/navegadores/firefox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# sudo sin contraseña
-
-echo ">>> Instalando Firefox..."
-sudo pacman -S --needed firefox --noconfirm
-
-echo ">>> Firefox instalado"
