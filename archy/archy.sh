1c524c68185d1cfbd17364b27b2644536aae1242
Edgares100IQ <edgar.08ds@gmail.com>
2026-04-25 21:30:31 +0200

    Update archy.sh

--- archy/archy.sh
+++ archy/archy.sh
@@ -501,6 +501,10 @@ while true; do
         1) menu_personalizadas ;;
         2) menu_desinstalar ;;
         3) menu_ajustes ;;
-        4) exit 0 ;;
+        4) clear
+           echo
+           echo "¡Espero que te haya servido de ayuda!"
+           echo
+           exit 0 ;;
     esac
 done
