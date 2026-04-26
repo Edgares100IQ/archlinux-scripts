522ac5684a0bad491a7b75cbbb79ddb88d268ccc
Edgares100IQ <edgar.08ds@gmail.com>
2026-04-26 13:50:13 +0200

    Update whatsapp.sh

--- archy/scripts/comunicacion/whatsapp.sh
+++ archy/scripts/comunicacion/whatsapp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo ">>> Instalando WhatsApp (WhatsDesk)..."
-yay -S --noconfirm --needed --answerclean All --answerdiff None whatsdesk-bin
+echo ">>> Instalando WhatsApp (Whatsie)..."
+yay -S --noconfirm --needed --answerclean All --answerdiff None whatsie
 echo ">>> WhatsApp instalado"
