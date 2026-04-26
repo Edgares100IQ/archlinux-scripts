4c706fa58e292cc64248a62b34ae3efd2000497c
Edgares100IQ <edgar.08ds@gmail.com>
2026-04-26 13:55:17 +0200

    Update telegram.sh

--- archy/scripts/comunicacion/telegram.sh
+++ archy/scripts/comunicacion/telegram.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo ">>> Actualizando mirrors..."
+sudo pacman -S --noconfirm --needed reflector
+sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
 echo ">>> Instalando Telegram..."
 sudo pacman -S --noconfirm --needed telegram-desktop
 echo ">>> Telegram instalado"
