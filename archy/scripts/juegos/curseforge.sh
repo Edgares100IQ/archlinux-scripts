ae87da5dc71f7ab031f38eed9b14d14ad6f64a8f
Edgares100IQ <edgar.08ds@gmail.com>
2026-05-02 18:56:31 +0200

    Add files via upload

---
+++ archy/scripts/juegos/curseforge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ">>> Instalando CurseForge..."
+yay -S --noconfirm --needed --answerclean All --answerdiff None curseforge
+
+echo ">>> CurseForge instalado"
---
+++ archy/scripts/juegos/modrinth.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ">>> Instalando Modrinth App..."
+yay -S --noconfirm --needed --answerclean All --answerdiff None modrinth-app
+
+echo ">>> Modrinth App instalada"
